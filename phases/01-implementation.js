130a14208a1ce66400027785c6913b1c0b0151a1
dcsiona@gmail.com <dcsiona@gmail.com>
2024-04-10 09:07:18 -0400

    another commit

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -84,12 +84,52 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   resize() {
     // Your code here
+    const oldData = this.data;
+    this.capacity *= 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    for (let i = 0; i < oldData.length; i++) {
+      let current = oldData[i];
+
+      while (current) {
+        this.insert(current.key, current.value);
+        current = current.next;
+      }
+    }
   }
 
 
   delete(key) {
     // Your code here
+    const index = this.hashMod(key);
+
+    let current = this.data[index];
+
+    if (!current) {
+      return null;
+    }
+
+    if (current.key === key) {
+      this.data[index] = current.next;
+      this.count--;
+      return current.value;
+    }
+
+    while (current.next) {
+      if (current.next.key === key) {
+        const value = current.next.value;
+        current.next = current.next.next;
+        this.count--;
+        return value;
+      }
+      current = current.next;
+    }
+
+    return null;
   }
+
+
 }
 
 
